888fe2ebe0e730b7448ccb175cf2cfeaab3a04a2
QuaternionWake <simep@proton.me>
2026-07-30 01:22:39 +0200

    Make buffer large enough to play higher bitrate tracks correctly

--- src/Player.zig
+++ src/Player.zig
@@ -109,12 +109,12 @@ stream: ?*sdl.AudioStream,
 pub fn play(self: *Player) !void {
     if (self.current_track >= self.tracks.len) return;
 
-    const min_queued = 10240;
+    const min_queued = 102400;
     if (!self.paused) {
         const stream = self.stream orelse return error.StreamUninitialized;
         const queued = try sdl.getAudioStreamQueued(stream);
         if (queued < min_queued) {
-            var buffer: [10240]f32 = undefined;
+            var buffer: [102400]f32 = undefined;
             const samples = try self.decoder.read(f32, &buffer);
             try sdl.putAudioStreamData(stream, f32, samples);
 
